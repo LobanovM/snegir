bad9d758c4e7f798513f02060113424d9365d552
Maksim <lobanovmv88@yandex.ru>
2023-08-28 21:27:10 +0400

    Add Serilog

--- src/Snegir.WebApp/Controllers/ContentsController.cs
+++ src/Snegir.WebApp/Controllers/ContentsController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+using Serilog.Extensions.Hosting;
 using Snegir.Core.Entities;
 using Snegir.Core.Services.Contents;
 using Snegir.Core.Types;
@@ -12,15 +17,20 @@ namespace Snegir.WebApp.Controllers
     {
         private IContentService _service;
 
-        public ContentsController(IContentService service, IContentService service2)
+
+        public ContentsController(ILogger<ContentsController> logger, IContentService service)
         {
             _service = service;
+            logger.LogInformation("ContentsController controller called ");
         }
 
         [HttpGet]
         public async Task<IEnumerable<Content>> Get()
         {
             var result = await _service.GetAll().ConfigureAwait(false);
+
+            var _serilogLogger = new LoggerConfiguration().CreateLogger();
+            _serilogLogger.Information("TEST INFO !!!!!!!!!!!!!!!!!");
 
             return result;
         }
--- src/Snegir.WebApp/Program.cs
+++ src/Snegir.WebApp/Program.cs
@@ -4,6 +4,8 @@ using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Snegir.WebApp.Util;
+using Serilog;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Snegir.WebApp
 {
@@ -11,32 +13,60 @@ namespace Snegir.WebApp
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
 
-            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-            builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacModule()));
 
-            builder.Services.AddMemoryCache();
-            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            builder.Services.AddReact();
-            builder.Services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName).AddChakraCore();
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            app.UseDeveloperExceptionPage();
-
-            app.UseReact(config => { });
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
+            Log.Information("Starting up!");
+            try
             {
-                endpoints.MapControllers();
-            });
 
-            app.Run();
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.UseSerilog((context, configuration) => 
+                    configuration.ReadFrom.Configuration(context.Configuration));
+
+                builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+                builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacModule()));
+
+                builder.Services.AddMemoryCache();
+                builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+                builder.Services.AddReact();
+                builder.Services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName).AddChakraCore();
+                builder.Services.AddControllers();
+
+                var app = builder.Build();
+
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                   // app.UseSwagger();
+                   // app.UseSwaggerUI();
+                }
+
+                app.UseSerilogRequestLogging();
+                app.UseReact(config => { });
+                app.UseDefaultFiles();
+                app.UseStaticFiles();
+                app.UseRouting();
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+
+                app.Run();
+
+                Log.Information("Stopped cleanly");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
